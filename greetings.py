11c9cad37a57deb7e9e4fd78a4ee37b5a74db0a2
SilensStorm <94792309+SilensStorm@users.noreply.github.com>
2025-01-20 07:41:29 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -10,9 +10,8 @@ print("Описание заметки",content)
 status = ("status")
 print("Сатус заметки",status)
 
-from datetime import date
-created_date = date.today()
-print("дата создания заметки в формате день-месяц-год ",created_date.day,".",created_date.month,".",created_date.year,sep="")
+created_date = ("created_date")
+print("дата создания заметки в формате день-месяц-год ",created_date)
 
 issue_date = ("issue_date")
 print("дата истечения заметки (дедлайн) в формате день-месяц-год",issue_date)
